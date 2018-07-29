91fc061a2d159f69cc509aa5ea56dcaad91294a5
vivek19patel <19rajpatel@gmail.com>
2018-07-29 23:06:48 +0530

    new basic core

---
+++ basic_core_java/src/main/java/com/vivek/core/Basic.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.vivek.core;
+
+public class Basic extends Thread {
+
+	Basic basic;
+	int x = 89;
+
+	@Override
+	public void run() {
+		synchronized (basic) {
+
+		}
+		System.out.println("run");
+	}
+
+	@Override
+	public synchronized void start() {
+		System.out.println("start");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		Basic basic = new Basic();
+		basic.start();
+
+		/*
+		 * try { System.out.println("hello"); Thread.sleep(1000); int x = 10;
+		 * System.exit(0); int y = x / 0; System.out.println(y); } catch
+		 * (Exception e) { System.out.println(e); } finally {
+		 * System.out.println("finally"); }
+		 */
+
+	}
+
+}
