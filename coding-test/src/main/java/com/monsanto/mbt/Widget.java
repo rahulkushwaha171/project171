906db02bfbdc2ece7fd463aaf2a2f511e0c18cc0
vivek19patel <19rajpatel@gmail.com>
2018-07-29 23:20:11 +0530

    coding test

---
+++ coding-test/src/main/java/com/monsanto/mbt/Widget.java
@@ -0,0 +1,46 @@
+package com.monsanto.mbt;
+
+import java.util.Date;
+
+public class Widget implements Comparable<Widget> {
+	private int serialNumber;
+	private String color;
+	private Date productionDate;
+
+	public Widget(int serialNumber, String color, Date productionDate) {
+		this.serialNumber = serialNumber;
+		this.color = color;
+		this.productionDate = productionDate;
+	}
+
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Date getProductionDate() {
+		return productionDate;
+	}
+
+	public void setProductionDate(Date productionDate) {
+		this.productionDate = productionDate;
+	}
+
+	public int compareTo(Widget o) {
+		int count = 0;
+		count = this.color.compareTo(o.color);
+		count = this.productionDate.compareTo(o.productionDate);
+		return count;
+	}
+}
