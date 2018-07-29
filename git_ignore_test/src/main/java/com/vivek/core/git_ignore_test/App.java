dec12fb57c875995c845578e561961141630d2bd
vivek19patel <19rajpatel@gmail.com>
2018-07-29 23:41:16 +0530

    git ignore

---
+++ git_ignore_test/src/main/java/com/vivek/core/git_ignore_test/App.java
@@ -0,0 +1,13 @@
+package com.vivek.core.git_ignore_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
